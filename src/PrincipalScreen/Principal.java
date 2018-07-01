bf93ed26fac30e1b177b41c663b2c8f0e058547b
CRISTIAN <CRISTIAN@192.168.11.4>
2018-06-30 18:25:34 -0600

    cambio de JPanel - JFrame

--- src/PrincipalScreen/Principal.java
+++ src/PrincipalScreen/Principal.java
@@ -12,7 +12,7 @@ package PrincipalScreen;
 public class Principal {
     
     public static void main(String[] args) {
-       new PrincipalScreen().setVisible(true);
+       new PantallaPrincipal().setVisible(true);
     }
     
 }
